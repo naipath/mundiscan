0465482c4235177d6dd402ec46ba075a1583bbe5
tvs <teije-vs@hotmail.com>
2017-07-27 01:24:20 +0200

    Implemented get Status

--- server/serve.go
+++ server/serve.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/naipath/mundiclient"
+	"encoding/json"
 )
 
 func main() {
@@ -14,6 +16,12 @@ func main() {
 	directory := flag.String("d", ".", "the directory of static file to host")
 	path := flag.String("path", "./test/", "The path to write file uploads to")
 	flag.Parse()
+
+	client, err := mundiclient.New("10.195.0.171", 1470)
+	if err != nil {
+		panic("Could not get a connection to Mundi laser")
+	}
+	defer client.Close()
 
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
 
@@ -28,16 +36,39 @@ func main() {
 		}
 		defer file.Close()
 		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile(*path+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		f, err := os.OpenFile(*path + handler.Filename, os.O_WRONLY | os.O_CREATE, 0666)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		defer f.Close()
 		io.Copy(f, file)
+	})
 
+	http.HandleFunc("/statusData", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		statusData, err := client.GetStatusData()
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("{\"error\": true}"))
+		} else {
+			json.NewEncoder(w).Encode(statusData)
+		}
+	})
+
+	http.HandleFunc("/statusMessage", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		statusMessage, err := client.GetStatusMessage()
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("{\"error\": true}"))
+		} else {
+			w.Write([]byte("{\"StatusMessage\": \"" + statusMessage + "\"}"))
+		}
 	})
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	log.Fatal(http.ListenAndServe(":" + *port, nil))
 }
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,290 @@
 <template>
     <div id="app" class="container">
 
-        <h1 class="title">Stap 1: Bovenkant</h1>
+        <nav class="navbar">
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" v-bind:class="{ ['is-active']: routes.laser }" href="#"
+                       @click.prevent="reroute('laser')">
+                        Laser
+                    </a>
+                </div>
 
-        <div v-if="showFileChooser">
-            <input type="file"
-                   accept="image/png,image/jpg,image/jpeg"
-                   v-on:change="chooseImage"/>
-        </div>
-
-        <hr/>
-
-        <div class="columns" v-if="!showFileChooser">
-            <div class="column is-half">
-                <button v-if="noTextAdded" v-on:click="addText" class="button is-info">Text toevoegen</button>
+                <div class="navbar-end">
+                    <a class="navbar-item" v-bind:class="{ ['is-active']: routes.settings }" href="#"
+                       @click.prevent="reroute('settings')">
+                        Settings
+                    </a>
+                </div>
             </div>
-            <div class="column is-half">
-                <button class="button is-success" v-on:click="onFinished">Klaar!</button>
+        </nav>
+
+
+        <div v-if="routes.laser">
+            <div v-if="showFileChooser">
+                <input type="file" accept="image/png,image/jpg,image/jpeg" v-on:change="chooseImage"/>
+            </div>
+
+            <hr v-if="showFileChooser"/>
+
+            <div class="columns" v-if="!showFileChooser">
+                <div class="column is-half">
+                    <button v-if="noTextAdded" v-on:click="addText" class="button is-info">Text toevoegen</button>
+                </div>
+                <div class="column is-half">
+                    <button class="button is-success is-pulled-right" v-on:click="onFinished">Klaar!</button>
+                </div>
+            </div>
+
+            <div class="column" v-if="!showFileChooser">
+                <canvas id="c" width="800" height="500"></canvas>
+            </div>
+
+            <a id="download" href="#" download="printer.png" style="display: none;"></a>
+        </div>
+
+        <div v-if="routes.settings">
+
+            <div class="columns">
+                <h1 class="title column">Status message</h1>
+                <div class="column">
+                    <button class="button is-info is-pulled-right" @click="retrieveMundiStatus">Refresh</button>
+                </div>
+            </div>
+            <input class="input" type="text" v-model="statusMessage" readonly>
+
+            <hr/>
+
+            <h1 class="title">Status data</h1>
+
+            <div class="columns">
+                <div class="column">
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.PrintStatus">
+                            Print status
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.FailureStatus">
+                            Failure status
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.WarningStatus">
+                            Warning status
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.MaintenanceStatus">
+                            Maintenance status
+                        </label>
+                    </div>
+                </div>
+                <div class="column">
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.DetectorFault">
+                            Detector fault
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.MarkNotLoaded">
+                            Mark not loaded
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.CodeParametersNotLoaded">
+                            Code parameters not loaded
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.TestingLaser">
+                            Testing laser
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.DisableShutter">
+                            Disable shutter
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.ExternalUpdateSingleShotNotUpdated">
+                            External update, single shot not updated
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.ComPortDisconnected">
+                            Com port disconnected
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.BarcodeError">
+                            Barcode error
+                        </label>
+                    </div>
+                </div>
+                <div class="column">
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.EStop">
+                            E stop
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.ExternalInterlocks">
+                            External interlocks
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.CoolantTemperature">
+                            Coolant temperature
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.DC24Volts">
+                            DC 24 volts
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.ShutterKeyswitch">
+                            Shutter keyswitch
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.Keyswitch">
+                            Keyswitch
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.DC48Volts">
+                            DC 48 volts
+                        </label>
+                    </div>
+
+
+                </div>
+                <div class="column">
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.CoolantFlow">
+                            Coolant Flow
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.EmissionIndicator">
+                            Emission indicator
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.ShutterPrint">
+                            Shutter print
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.ShutterStandby">
+                            Shutter standby
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.VSWRError">
+                            VSWR error
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.OverModulation">
+                            Over modulation
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.TachoFault">
+                            Tacho fault
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.RFStatus">
+                            RF status
+                        </label>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.SystemEnable">
+                            System enable
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.VapourExtractorFault">
+                            Vapour extractor fault
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.GalvoPower">
+                            Galvo power
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.GalvoTemperature">
+                            Galvo temperature
+                        </label>
+                    </div>
+                    <div>
+                        <label class="checkbox">
+                            <input type="checkbox" @click.prevent v-model="statusData.GalvoCableDisconnected">
+                            Galvo cable disconnected
+                        </label>
+                    </div>
+                </div>
+
             </div>
         </div>
-
-        <div class="column" v-if="!showFileChooser">
-            <canvas id="c" width="300" height="300"></canvas>
-        </div>
-
-        <a id="download" href="#" download="printer.png" style="display: none;"></a>
-
     </div>
 </template>
 
@@ -39,6 +298,14 @@
             return {
                 showFileChooser: true,
                 noTextAdded: true,
+
+                routes: {
+                    laser: true,
+                    settings: false,
+                },
+
+                statusMessage: "Press refresh to obtain a new status",
+                statusData: {}
             }
         },
         methods: {
@@ -82,6 +349,31 @@
                 } else {
                     alert('The File APIs are not fully supported in this browser.');
                 }
+            },
+
+            retrieveMundiStatus: function () {
+                fetch("/statusMessage")
+                    .then(response => response.json())
+                    .then(msg => this.statusMessage = msg.StatusMessage)
+
+                setTimeout(() => fetch("/statusData")
+                    .then(response => response.json())
+                    .then(msg => this.statusData = msg), 400)
+            },
+
+            reroute: function (route) {
+                switch (route) {
+                    case "laser":
+                        this.routes.laser = true
+                        this.routes.settings = false
+                        break;
+
+                    case "settings":
+                        this.routes.laser = false
+                        this.routes.settings = true
+                        this.retrieveMundiStatus()
+                        break;
+                }
             }
         },
         mounted() {
@@ -95,13 +387,11 @@
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
     }
 
-    h1, h2 {
-        font-weight: normal;
+    .navbar {
+        border-bottom: 1px solid #eee;
+        margin-bottom: 50px;
     }
 
     #c {
