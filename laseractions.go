188b94b4ff65a0ba3a9c4cf9fa035b218c1d2c22
tvs <teije-vs@hotmail.com>
2017-08-10 21:12:35 +0200

    Split the interface and logic from each other

---
+++ laseractions.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"os"
+	"github.com/go-chi/chi"
+	"io"
+	"image/png"
+	"github.com/nfnt/resize"
+	"golang.org/x/image/bmp"
+	"github.com/naipath/mundiclient"
+)
+
+type LaserClient struct {
+	client *mundiclient.MundiClient
+	Name   string
+	Ip     string
+	Port   int
+}
+
+var laserClients = make([]LaserClient, 0)
+
+type CreateLaserClientRequest struct {
+	Name string
+	Ip   string
+	Port int
+}
+
+type LaserStatusRequest struct {
+	Message string
+	Status  mundiclient.StatusData
+}
+
+func getLaserClient(r *http.Request) LaserClient {
+	laserClientName := chi.URLParam(r, "laserClientName")
+	for _, x := range laserClients {
+		if x.Name == laserClientName {
+			return x
+		}
+	}
+	return LaserClient{}
+}
+
+func convertPngToBmp(reader io.Reader, fileName string) error {
+	img, decodeErr := png.Decode(reader)
+
+	resizedImg := resize.Resize(210, 210, img, resize.Lanczos3)
+
+	if decodeErr != nil {
+		return decodeErr
+	}
+	f, createErr := os.Create(fileName)
+	defer f.Close()
+	if createErr != nil {
+		return createErr
+	}
+	bmp.Encode(f, resizedImg)
+	return nil
+}
+
+func getLaserClients(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(laserClients)
+}
+
+func addLaserClient(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	var data CreateLaserClientRequest
+	decodeErr := decoder.Decode(&data)
+	if decodeErr != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("{\"error\": \"Could not decode json request\"}"))
+	}
+	newClient, clientErr := mundiclient.New(data.Ip, data.Port)
+	if clientErr != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\": \"Could not connect to Mundi laser\"}"))
+	} else {
+		newLaserClient := LaserClient{newClient, data.Name, data.Ip, data.Port}
+		laserClients = append(laserClients, newLaserClient)
+
+		w.WriteHeader(201)
+		json.NewEncoder(w).Encode(newLaserClient)
+	}
+}
+
+func deleteLaserClient(w http.ResponseWriter, r *http.Request) {
+	client := getLaserClient(r)
+	client.client.Close()
+
+	newClients := make([]LaserClient, 0)
+	for _, v := range laserClients {
+		if v.Name != client.Name {
+			newClients = append(newClients, v)
+		}
+	}
+	laserClients = newClients
+	w.WriteHeader(204)
+}
+
+func getLaserClientCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	laserClient := getLaserClient(r)
+	counters, err := laserClient.client.GetCounters()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\": \"Could not retrieve counters from mundiclient\"}"))
+	} else {
+		json.NewEncoder(w).Encode(counters)
+	}
+}
+
+func resetLaserClientCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	laserClient := getLaserClient(r)
+	err := laserClient.client.ResetCurrentCount()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\": \"Could not retrieve counters from mundiclient\"}"))
+	}
+	w.WriteHeader(204)
+}
+
+func getLaserClientStatus(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	laserClient := getLaserClient(r)
+	statusData, statusDataErr := laserClient.client.GetStatusData()
+	statusMessage, statusMessageErr := laserClient.client.GetStatusMessage()
+
+	if statusDataErr != nil || statusMessageErr != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\": \"Could not retrieve status from mundiclient\"}"))
+	}
+	json.NewEncoder(w).Encode(LaserStatusRequest{statusMessage, statusData})
+}
+
+func uploadLogoToLaser(w http.ResponseWriter, r *http.Request) {
+	client := getLaserClient(r)
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("{\"error\": \"Could not retrieve formfield uploadfile\"}"))
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintf(w, "%v", handler.Header)
+
+	fileName := *path + handler.Filename[:len(handler.Filename)-4] + ".bmp"
+	convertErr := convertPngToBmp(file, fileName)
+	if convertErr != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\": \"Could not convert file to bmp\"}"))
+		fmt.Println(convertErr)
+		return
+	}
+
+	logo, _ := os.Open(fileName)
+
+	uploadErr := client.client.UploadLogo(logo)
+	if uploadErr != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\": \"Could not upload file to mundiclient\"}"))
+		fmt.Println(uploadErr)
+		return
+	}
+	file.Close()
+}
--- serve.go
+++ serve.go
@@ -2,162 +2,36 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"github.com/go-chi/chi"
 	"log"
 	"net/http"
-	"os"
-	"github.com/naipath/mundiclient"
-	"encoding/json"
-	"image/png"
-	"golang.org/x/image/bmp"
-	"io"
-	"github.com/nfnt/resize"
-	"github.com/go-chi/chi"
 )
 
-type LaserClient struct {
-	client *mundiclient.MundiClient
-	Name   string
-	Ip     string
-	Port   int
-}
-
-var laserClients = make([]LaserClient, 0)
-
-type CreateLaserClientRequest struct {
-	Name string
-	Ip   string
-	Port int
-}
-
-type LaserStatusRequest struct {
-	Message string
-	Status  mundiclient.StatusData
-}
+var (
+	path *string
+)
 
 func main() {
 	port := flag.String("p", "8100", "port to serve on")
 	directory := flag.String("d", ".", "the directory of static file to host")
-	path := flag.String("path", "./", "The path to write file uploads to")
+	path = flag.String("path", "./", "The path to write file uploads to")
 	flag.Parse()
 
 	r := chi.NewRouter()
 
 	r.Route("/laserclients", func(r chi.Router) {
-
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(laserClients)
-		})
-
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-
-			decoder := json.NewDecoder(r.Body)
-			var data CreateLaserClientRequest
-			decodeErr := decoder.Decode(&data)
-			if decodeErr != nil {
-				w.WriteHeader(400)
-				w.Write([]byte("{\"error\": \"Could not decode json request\"}"))
-			}
-			newClient, clientErr := mundiclient.New(data.Ip, data.Port)
-			if clientErr != nil {
-				w.WriteHeader(500)
-				w.Write([]byte("{\"error\": \"Could not connect to Mundi laser\"}"))
-			} else {
-				newLaserClient := LaserClient{newClient, data.Name, data.Ip, data.Port}
-				laserClients = append(laserClients, newLaserClient)
-
-				w.WriteHeader(201)
-				json.NewEncoder(w).Encode(newLaserClient)
-			}
-		})
+		r.Get("/", getLaserClients)
+		r.Post("/", addLaserClient)
 
 		r.Route("/{laserClientName}", func(r chi.Router) {
-			r.Delete("/", func(w http.ResponseWriter, r *http.Request) {
-				b := laserClients[:0]
-				client := getLaserClient(r)
-				client.client.Close()
-				b = append(b, client)
-				laserClients = b
-
-				w.WriteHeader(204)
-			})
+			r.Delete("/", deleteLaserClient)
+			r.Get("/status", getLaserClientStatus)
+			r.Post("/upload", uploadLogoToLaser)
 
 			r.Route("/count", func(r chi.Router) {
-				r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-					w.Header().Set("Content-Type", "application/json")
-
-					laserClient := getLaserClient(r)
-					counters, err := laserClient.client.GetCounters()
-					if err != nil {
-						w.WriteHeader(500)
-						w.Write([]byte("{\"error\": \"Could not retrieve counters from mundiclient\"}"))
-					} else {
-						json.NewEncoder(w).Encode(counters)
-					}
-				})
-
-				r.Delete("/", func(w http.ResponseWriter, r *http.Request) {
-					w.Header().Set("Content-Type", "application/json")
-
-					laserClient := getLaserClient(r)
-					err := laserClient.client.ResetCurrentCount()
-					if err != nil {
-						w.WriteHeader(500)
-						w.Write([]byte("{\"error\": \"Could not retrieve counters from mundiclient\"}"))
-					}
-					w.WriteHeader(204)
-				})
+				r.Get("/", getLaserClientCount)
+				r.Delete("/", resetLaserClientCount)
 			})
-
-			r.Get("/status", func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("Content-Type", "application/json")
-
-				laserClient := getLaserClient(r)
-				statusData, statusDataErr := laserClient.client.GetStatusData()
-				statusMessage, statusMessageErr := laserClient.client.GetStatusMessage()
-
-				if statusDataErr != nil || statusMessageErr != nil {
-					w.WriteHeader(500)
-					w.Write([]byte("{\"error\": \"Could not retrieve status from mundiclient\"}"))
-				}
-				json.NewEncoder(w).Encode(LaserStatusRequest{statusMessage, statusData})
-			})
-
-			r.Post("/upload", func(w http.ResponseWriter, r *http.Request) {
-				client := getLaserClient(r)
-				r.ParseMultipartForm(32 << 20)
-				file, handler, err := r.FormFile("uploadfile")
-				if err != nil {
-					w.WriteHeader(400)
-					w.Write([]byte("{\"error\": \"Could not retrieve formfield uploadfile\"}"))
-					fmt.Println(err)
-					return
-				}
-				fmt.Fprintf(w, "%v", handler.Header)
-
-				fileName := *path + handler.Filename[:len(handler.Filename)-4] + ".bmp"
-				convertErr := convertPngToBmp(file, fileName)
-				if convertErr != nil {
-					w.WriteHeader(500)
-					w.Write([]byte("{\"error\": \"Could not convert file to bmp\"}"))
-					fmt.Println(convertErr)
-					return
-				}
-
-				logo, _ := os.Open(fileName)
-
-				uploadErr := client.client.UploadLogo(logo)
-				if uploadErr != nil {
-					w.WriteHeader(500)
-					w.Write([]byte("{\"error\": \"Could not upload file to mundiclient\"}"))
-					fmt.Println(uploadErr)
-					return
-				}
-				file.Close()
-			})
-
 		})
 	})
 
@@ -166,32 +40,5 @@ func main() {
 	}))
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-	log.Fatal(http.ListenAndServe(":" + *port, r))
-}
-
-func getLaserClient(r *http.Request) LaserClient {
-	laserClientName := chi.URLParam(r, "laserClientName")
-	for _, x := range laserClients {
-		if x.Name == laserClientName {
-			return x
-		}
-	}
-	return LaserClient{}
-}
-
-func convertPngToBmp(reader io.Reader, fileName string) (error) {
-	img, decodeErr := png.Decode(reader)
-
-	resizedImg := resize.Resize(210, 210, img, resize.Lanczos3)
-
-	if decodeErr != nil {
-		return decodeErr
-	}
-	f, createErr := os.Create(fileName)
-	defer f.Close()
-	if createErr != nil {
-		return createErr
-	}
-	bmp.Encode(f, resizedImg)
-	return nil
+	log.Fatal(http.ListenAndServe(":"+*port, r))
 }
